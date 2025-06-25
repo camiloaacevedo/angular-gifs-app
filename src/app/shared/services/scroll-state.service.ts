2d7af7d63da1366ead1ddd69a902b9f1a63cc0ba
CamiloAcevedoCuevas <ccuevasa@uninorte.edu.co>
2025-06-25 09:47:14 -0500

    .

--- src/app/shared/services/scroll-state.service.ts
+++ src/app/shared/services/scroll-state.service.ts
@@ -3,4 +3,11 @@ import { Injectable, signal } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class ScrollStateService {
   trendfingScrollState = signal(0);
+
+  // pagesScrollState: Record<string, number> = {
+  //   'page1': 1000,
+  //   'page2': 0,
+  //   'aboutPage': 50,
+  //   'page20': 0,
+  // }
 }
