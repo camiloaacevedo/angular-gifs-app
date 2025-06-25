1686fda7e217ba0f8c590d56c61405f05378c5bf
CamiloAcevedoCuevas <ccuevasa@uninorte.edu.co>
2025-06-25 03:34:29 -0500

    .

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,7 @@ const GIF_KEY = 'gifs';
 const loadFromLocalStorage = () => {
   const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}'; //Record<string, gifs[]>
   const gifs = JSON.parse(gifsFromLocalStorage);
-  console.log(gifs);
+
   return gifs;
 };
 
@@ -28,8 +28,19 @@ const loadFromLocalStorage = () => {
 export class GifService {
   private http = inject(HttpClient);
 
-  trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading = signal(true);
+  trendingGifs = signal<Gif[]>([]); //[gif,gif,gif,gif,gif,gif,]
+  trendingGifsLoading = signal(false);
+  private trendingPage = signal(0);
+
+  // [ [gif,gif,gif,], [gif,gif,gif,],[gif,gif,gif,],[gif,gif,gif,] ]
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+
+    return groups; // [ [g1,g2,g3],[g4,g5]]
+  });
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
@@ -44,18 +55,24 @@ export class GifService {
   });
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
         params: {
           api_key: environment.giphyApiKey,
           limit: 20,
+          offset: this.trendingPage() * 20,
         },
       })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-        this.trendingGifs.set(gifs);
+        this.trendingGifs.update((currentGifs) => [...currentGifs, ...gifs]);
+        this.trendingPage.update((page) => page + 1);
+
         this.trendingGifsLoading.set(false);
-        console.log({ gifs });
       });
   }
 
@@ -73,8 +90,8 @@ export class GifService {
         map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
 
         // Historial
-        tap(items => {
-          this.searchHistory.update(history => ({
+        tap((items) => {
+          this.searchHistory.update((history) => ({
             ...history,
             [query.toLowerCase()]: items,
           }));
